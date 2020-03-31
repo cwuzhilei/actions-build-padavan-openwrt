00645e9fcd70d118e394060e49a5094bb33a52f6
cwuzhilei <62702481+cwuzhilei@users.noreply.github.com>
2020-03-31 16:41:56 +0800

    Update public.sh

--- padavan/public/public.sh
+++ padavan/public/public.sh
@@ -4,7 +4,7 @@
 
 user_name='admin'                        # 用户名 admin
 user_password=$user_name                 # 用户密码 admin
-lan_ip='192.168.3'                       # lan 地址 192.168.3.1 一定别写后面的 .1
+lan_ip='192.168.2'                       # lan 地址 192.168.3.1 一定别写后面的 .1
 wifi_password='1234567890'               # wifi密码，切记密码最少8位 admin
 version_time=$(date "+%Y%m%d")           # 自动时间更新时版本号: 20200320
 default_path='./user/shared/defaults.h'  # 默认文件配置目录
